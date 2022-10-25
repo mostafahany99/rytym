6ca38f73f4a1b773f8544c1d6c04173a8eddd5c4
mostafahany99 <114784979+mostafahany99@users.noreply.github.com>
2022-10-25 21:02:59 +0200

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,18 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'MTAxOTY4NjYxMjM0NDUwODQ1Nw.GkhZ5Y.pYJT63JVdiG-t3GwuObMtAEzNGjJS1RcZPXjRU',
+        prefix: '+',
+        activity: '',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
+
